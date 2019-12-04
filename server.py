85f4bb1638b9571b7157560da845fa3315f001ec
Cory Lewis <cjl9703@yahoo.com>
2019-12-03 22:33:10 -0800

    (add) site logic and connected to database

--- server.py
+++ server.py
@@ -1,20 +1,38 @@
 from flask import Flask
 from flask import Flask, render_template, request, redirect
 import requests
-import sqlite3
-
-conn = sqlite3.connect('./db/main.db')
-db = conn.cursor()
 app = Flask(__name__)
+import pymysql
 
-def query(string):
-    db.execute(string) # run SQL query
-    conn.commit() # save changes
-    return db.fetchone() # returns None if query is empty
+class Database:
+    def __init__(self):
+        host = "rds-mysql-csc675.c4pugfa5ehgs.us-west-1.rds.amazonaws.com"
+        user = "CSC675Database"
+        password = "management"
+        db = "sys"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
+                                   DictCursor)
+        self.cur = self.con.cursor()
+    def query(self, string):
+        self.cur.execute(str(string))
+        return self.cur.fetchall()
 
-def generate_db():
-    query('''CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)''') # Create table
-    query("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)") # Insert a row of data
+    def getEmployees(self):
+        return self.query("SELECT * FROM sys.Employee;")
+    def getOrders(self):
+        return self.query("SELECT * FROM sys.Orders;")
+    def getMenu(self):
+        return self.query("SELECT * FROM sys.Menu;")
+    def getMenuItems(self):
+        return self.query("SELECT * FROM sys.Menu_Item;")
+    def getEmployeeTypes(self):
+        return self.query("SELECT * FROM sys.Employee_Type;")
+    def getInventory(self):
+        return self.query("SELECT * FROM sys.Inventory;")
+    def completeOrder(self, order_id):
+        self.query("UPDATE Orders O SET O.Order_is_complete = 1 WHERE O.order_id = " + order_id + ";")
+
+db = Database()
 
 @app.route("/")
 def home():
@@ -22,43 +40,61 @@ def home():
 
 @app.route("/main")
 def main():
-    return render_template("index.html")
+    return render_template("index.html", menu = db.getMenu())
 
-@app.route("/add", methods=["POST"])
-def add():
-    # sample = request.form.get("sample")
-    return redirect("/main")
-#     # pm = ProxyManager(PATH_TO_DATABASE) 
-    
-#     return render_template("proxy-settings.html",
-#                             cached_sites = pm.list_of_cached_sites(),
-#                             blocked_sites = pm.list_of_blocked_sites(),
-#                             admins = pm.list_of_admins(),
-#                             private_mode_users = pm.list_of_managers(),
-#                             manager_sites = pm.list_of_manager_sites())
+@app.route("/employee")
+def employee():
+    return render_template("employee.html", 
+                            employees = db.getEmployees(), 
+                            employee_types = db.getEmployeeTypes() ) 
 
+@app.route("/order")
+def order():
+    return render_template("order.html", 
+                            orders = db.getOrders() ) 
 
-@app.route("/home.html", methods=["POST"])
-def get_user_input():
-    # fetch user credentials
-    # username = request.form.get("username")
-    # password = request.form.get("password")
+@app.route("/inventory")
+def inventory():
+    return render_template("inventory.html", 
+                            inventory = db.getInventory() ) 
 
-    # url = request.form.get("url")
-    # is_private_mode = 0
-    # if request.form.get("private"):
-    #     is_private_mode = 1
-    # if "proxy-settings" in url:
-    #     return proxy_settings()
-    # data = {"url": url, "is_private_mode": is_private_mode, "username": username, "password": password}
-    # client = Client()
-    # client.request_to_proxy(data)
+@app.route("/order", methods=["POST"])
+def place_order():
+    order_sample = request.form.get("order_sample")
+    print("Sample output: " + str(order_sample))
 
-    # return client.response_from_proxy()
+    return redirect("/order")
 
-    return ""
+@app.route("/complete_order", methods=["POST"])
+def complete_order():
+    order_id = request.form.get("order_id")
 
+    print("id: " + str(order_id))
+
+    return redirect("/order")
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+
+
+
+
+# import sqlite3
+
+# conn = sqlite3.connect('./db/main.db')
+# db = conn.cursor()
+
+# def query(string):
+#     db.execute(string) # run SQL query
+#     conn.commit() # save changes
+#     return db.fetchone() # returns None if query is empty
+
+# def generate_db():
+#     query('''CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)''') # Create table
+#     query("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)") # Insert a row of data
 
